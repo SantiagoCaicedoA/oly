222d0943dc4d6093ccfed282a14d2acf7a24e9f1
codewithhannan <hannan404@gmail.com>
2026-01-28 20:35:14 +0500

    UI of tab home screen

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,75 @@
+import { Images } from "@/assets";
+import PostCard from "@/components/post-card";
+import { useTheme } from "@/context/theme-context";
+import { Typography } from "@/utils/custom-styles";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { scale } from "react-native-size-matters";
 
 export default function Home() {
+  const { colors } = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+    },
+    scrollContent: {
+      paddingVertical: scale(15),
+      paddingHorizontal: scale(14),
+    },
+    icon: {
+      width: scale(20),
+      height: scale(20),
+      borderRadius: scale(20),
+      marginRight: scale(10),
+    },
+    header: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      backgroundColor: "#0f1b25",
+      paddingVertical: scale(12),
+      borderBottomWidth: 0.5,
+      borderColor: colors.text,
+
+      paddingHorizontal: scale(8),
+    },
+    headerSection: {
+      flexDirection: "row",
+    },
+    home: {
+      fontSize: Typography.fontSize.lg,
+      fontWeight: Typography.fontWeight.normal,
+      letterSpacing: Typography.letterSpacing.normal,
+      color: colors.text,
+    },
+  });
+
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerSection}>
+          <Image source={Images.profile} style={styles.icon} />
+          <Image source={Images.search} style={styles.icon} />
+        </View>
+
+        <Text style={styles.home}>HOME</Text>
+        <View style={styles.headerSection}>
+          <Image source={Images.notificationicon} style={styles.icon} />
+          <Image source={Images.comment} style={styles.icon} />
+        </View>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false}
+      >
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({});
--- components/post-card.tsx
+++ components/post-card.tsx
@@ -1,8 +1,120 @@
+import { Images } from "@/assets";
+import { useTheme } from "@/context/theme-context";
+import { Typography } from "@/utils/custom-styles";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { scale } from "react-native-size-matters";
 
-export default function Postcard() {
-  return <View></View>;
+export default function PostCard() {
+  const { colors } = useTheme();
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: colors.background,
+      borderRadius: scale(12),
+      borderColor: colors.text,
+      borderWidth: 0.3,
+      padding: scale(12),
+      marginBottom: scale(15),
+    },
+    header: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: scale(6),
+    },
+    profile: {
+      width: scale(40),
+      height: scale(40),
+      borderRadius: scale(20),
+      marginRight: scale(10),
+    },
+    userInfo: {
+      flex: 1,
+    },
+    name: {
+      fontSize: Typography.fontSize.lg,
+      fontWeight: Typography.fontWeight.normal,
+      letterSpacing: Typography.letterSpacing.normal,
+      color: colors.text,
+    },
+    userName: {
+      fontSize: Typography.fontSize.base,
+      fontWeight: Typography.fontWeight.normal,
+      letterSpacing: Typography.letterSpacing.normal,
+      color: colors.textSecondary,
+    },
+    caption: {
+      fontSize: Typography.fontSize.md,
+      fontWeight: Typography.fontWeight.normal,
+      letterSpacing: Typography.letterSpacing.normal,
+      color: colors.textSecondary,
+      marginBottom: scale(12),
+    },
+    image: {
+      width: "100%",
+      height: scale(400),
+      borderRadius: scale(12),
+    },
+    icon: {
+      width: scale(18),
+      height: scale(18),
+    },
+    iconContainer: {
+      flexDirection: "row",
+      gap: scale(9),
+      alignItems: "center",
+      marginTop: scale(12),
+    },
+    count: {
+      fontSize: Typography.fontSize.base,
+      fontWeight: Typography.fontWeight.normal,
+      letterSpacing: Typography.letterSpacing.normal,
+      color: colors.textSecondary,
+    },
+    arrowForward: {
+      width: scale(7),
+      height: scale(7),
+    },
+    time: {
+      fontSize: Typography.fontSize.base,
+      fontWeight: Typography.fontWeight.normal,
+      letterSpacing: Typography.letterSpacing.normal,
+      color: colors.textSecondary,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={Images.profile} style={styles.profile} />
+        <View style={styles.userInfo}>
+          <View
+            style={{
+              flexDirection: "row",
+              gap: scale(7),
+              alignItems: "center",
+            }}
+          >
+            <Text style={styles.name}>Athlete name</Text>
+            <Text style={styles.time}>6m</Text>
+          </View>
+
+          <Text style={styles.userName}>@username</Text>
+        </View>
+        <Image source={Images.arrowforward} style={styles.arrowForward} />
+      </View>
+
+      <Text style={styles.caption}>
+        Third complex rep out of four at 103 kg. Focused on staying smooth and
+        consistent throughout the set.
+      </Text>
+      <Image source={Images.man} style={styles.image} resizeMode="cover" />
+      <View style={styles.iconContainer}>
+        <Image source={Images.like} style={styles.icon} />
+        <Text style={styles.count}>12</Text>
+        <Image source={Images.comment} style={styles.icon} />
+        <Text style={styles.count}>3</Text>
+      </View>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({});
